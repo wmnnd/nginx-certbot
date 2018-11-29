2c9169d95ec3959219ed0467c950c944c12c2cfb
Yaroslav <hello@unimarijo.com>
2018-11-29 18:56:59 +0400

    Executing nginx reload only after everything

--- certbot.sh
+++ certbot.sh
@@ -67,5 +67,6 @@ for domain in "${domains[@]}"; do
   mkdir -p "$data_path/www"
   docker-compose run --rm --entrypoint "certbot certonly --webroot -w /var/www/certbot -d $domain \
   $staging_arg $email_arg --rsa-key-size $rsa_key_size --agree-tos --force-renewal" certbot
-  docker-compose exec nginx nginx -s reload
 done
+
+docker-compose exec nginx nginx -s reload
