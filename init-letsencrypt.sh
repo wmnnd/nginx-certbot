07f113d3d8375b73d313d940002f99273b6f1173
Philipp <github@vanbittern.com>
2019-01-01 22:07:00 +0100

    Add newline for output clarity

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -19,6 +19,7 @@ if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/
   mkdir -p "$data_path/conf"
   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+  echo
 fi
 
 
