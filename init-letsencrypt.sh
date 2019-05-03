2ab4744710cf14d3ce8bea4d3c6359d57cc3d85b
Yossi Nixon <ynixon@gmail.com>
2019-05-03 11:23:05 +0300

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(localhost YossiNi-Lap-AX)
+domains=(example.com www.example.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
