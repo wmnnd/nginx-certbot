9d31dae19de502487e87e403d327bc293eaf14e3
Yossi Nixon <ynixon@gmail.com>
2019-05-03 07:56:00 +0000

    added execute permissions to init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.com www.example.com)
+domains=(localhost YossiNi-Lap-AX)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
