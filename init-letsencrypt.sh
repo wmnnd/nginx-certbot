86dd7652d96f2f54965e1a4bd34db07282e427f3
Daniel Okey-Okoro <daniel.okeyokoro@britecore.com>
2019-10-21 19:26:13 -0700

    feat: remove `docker-compose` as a dependency
    I had trouble deploying this setup on a GCP VM
    running their "Container-Optimized OS"
    
    See reasons here: https://cloud.google.com/community/tutorials/docker-compose-on-container-optimized-os
    
    Refactoring this script allowed me to get up & running.
    
    I think if you accepted these changes,
    You would make the software more accessible

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
+function dc() {
+  # run docker-compose in docker
+  docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w="$PWD" docker/compose:1.24.0 "$@"
+  # source: https://cloud.google.com/community/tutorials/docker-compose-on-container-optimized-os
+}
+
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Error: docker is not installed.' >&2
   exit 1
 fi
 
@@ -30,7 +36,7 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
+dc run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:1024 -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -39,11 +45,11 @@ echo
 
 
 echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
+dc up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "\
+dc run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -66,7 +72,7 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose run --rm --entrypoint "\
+dc run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
@@ -77,4 +83,4 @@ docker-compose run --rm --entrypoint "\
 echo
 
 echo "### Reloading nginx ..."
-docker-compose exec nginx nginx -s reload
+dc exec nginx nginx -s reload
