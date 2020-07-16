ad6258f848235285961fb6a64c022fae3e5ba194
Emily Gorcenski <Gorcenski@users.noreply.github.com>
2020-07-16 13:45:34 +0200

    Keep rsa key size consistent
    Closes #73

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -31,7 +31,7 @@ echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
 docker-compose run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:1024 -days 1\
+  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
     -subj '/CN=localhost'" certbot
