44a789c02b4d6c8b97cb2a94a4e50ed3546341b9
alexandrepetrillo <alexandre.petrillo@gmail.com>
2020-12-30 21:17:54 +0100

    horodateur.me

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(parkingmoinscon.com www.parkingmoinscon.com)
+domains=(horodateur.me www.horodateur.me)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="contact@parkingmoinscon.com" # Adding a valid address is strongly recommended
+email="contact@horodateur.me" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
