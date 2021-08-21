034c6e61d7a98aab8113418ea7df7c1487ed4cc9
woraphol <woraphol.j@linecorp.com>
2021-08-21 20:58:37 +0700

    feat: add email

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(data-scrapper.efficient.guru)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="woraphol.j@appwow.co" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
