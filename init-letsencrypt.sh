7a992d8c4a63c7a7da942a60dff838d24357e676
woraphol <woraphol.j@linecorp.com>
2021-08-21 20:55:51 +0700

    feat: add TLS for-data-scrapper-efficient-guru

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org www.example.org)
+domains=(data-scrapper.efficient.guru)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
