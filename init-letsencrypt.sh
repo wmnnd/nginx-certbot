e4f697c2f44c64a92fdff947b0812239c0c8790b
Luke 10X <spam@luke10x.dev>
2023-03-20 19:57:33 -0400

    🎉 Awesome Nginx Certbot Compose! with some improvements
    - domain names are figured out from /etc/nginx/conf.d/*.conf filenames;
    - top-level directories in repo represent Compose services (for
      autocomplete);
    - static websites will be contained in a mounted directory
      /var/www/websites.

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,9 +5,18 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org www.example.org)
+function lsconf() {
+  for file in ./nginx/conf.d/*.conf; do
+      filename=$(basename "$file")
+      echo "${filename%.*}"
+  done
+}
+
+configs=$(lsconf | xargs)
+domains=($configs)
+
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="./certbot/letsencrypt"
 email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
@@ -19,17 +28,17 @@ if [ -d "$data_path" ]; then
 fi
 
 
-if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
+if [ ! -e "$data_path/options-ssl-nginx.conf" ] || [ ! -e "$data_path/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
-  mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+  mkdir -p "$data_path"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/ssl-dhparams.pem"
   echo
 fi
 
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
+mkdir -p "$data_path/live/$domains"
 docker-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
